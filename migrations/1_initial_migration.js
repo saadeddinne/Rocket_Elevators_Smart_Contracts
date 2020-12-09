d6aae1a0fee2a128abc1237306bec0f29fbc2715
WillJacques <68091327+WillJacques@users.noreply.github.com>
2020-12-08 20:05:10 -0500

    new start

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-var ProjectOffice = artifacts.require("ProjectOffice");
-
-module.exports = function (deployer) {
-	deployer.deploy(Migrations);
-	deployer.deploy(ProjectOffice);
-};

7d9d5cc07d74c10a9eca5c5ca6df380b771755f4
saadeddine <saadeddine.feki@gmail.com>
2020-12-10 07:49:37 -0500

    from String to uint

--- client/src/App.js
+++ client/src/App.js
@@ -18,11 +18,6 @@ class App extends Component {
 
 	componentDidMount() {
 		const { drizzle } = this.props;
-		this.controller = React.createRef();
-		this.door = React.createRef();
-		this.state = {
-			value: "",
-		};
 
 		// subscribe to changes in the store
 		this.unsubscribe = drizzle.store.subscribe(() => {
@@ -48,11 +43,6 @@ class App extends Component {
 		document.body.appendChild(script2);
 	}
 
-	handleChange = (e) => {
-		alert(e.target.controller.value);
-		this.setState({ value: this.controller.current.value });
-	};
-
 	componentWillUnmount() {
 		this.unsubscribe();
 	}
@@ -71,74 +61,37 @@ class App extends Component {
 											data-toggle="collapse"
 											data-parent="#accordion"
 											href="#collapseOne"
+											><span className="glyphicon glyphicon-file"> </span>NEW ORDER</a
 										>
-											<span className="glyphicon glyphicon-file"> </span>NEW
-											ORDER
-										</a>
 									</h4>
-
-									<h3>Elevator: {this.controller.value}</h3>
-									<h3>Door: {this.door.value}</h3>
 								</div>
-
 								<div id="collapseOne" className="panel-collapse collapse in">
 									<div className="panel-body">
 										<div className="row">
 											<div className="col-md-12">
-												<h3>Value controller: {this.state.value}</h3>
-												<form>
-													<div className="form-group">
-														Controller needed :{" "}
-														<SetController
-															drizzle={this.props.drizzle}
-															drizzleState={this.state.drizzleState}
-															ref={this.controller}
-															onChange={this.handleChange}
-														/>
-													</div>
-													<div className="form-group">
-														Cages needed :{" "}
-														<SetCage
-															drizzle={this.props.drizzle}
-															drizzleState={this.state.drizzleState}
-														/>
-													</div>
-													<div className="form-group">
-														Motors needed :{" "}
-														<SetMotor
-															drizzle={this.props.drizzle}
-															drizzleState={this.state.drizzleState}
-														/>
-													</div>
-													<div className="form-group">
-														Doors needed :{" "}
-														<SetDoor
-															drizzle={this.props.drizzle}
-															drizzleState={this.state.drizzleState}
-														/>
-													</div>
-													<div className="form-group">
-														Buttons needed :{" "}
-														<SetButton
-															drizzle={this.props.drizzle}
-															drizzleState={this.state.drizzleState}
-														/>
-													</div>
-													<div className="form-group">
-														Displays needed :{" "}
-														<SetDisplay
-															drizzle={this.props.drizzle}
-															drizzleState={this.state.drizzleState}
-														/>
-													</div>
-													<button>Submit</button>
-												</form>
+												<div className="form-group">
+													Controller needed : <SetController drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
+												</div>
+												<div className="form-group">
+													Cages needed : <SetCage drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
+												</div>
+												<div className="form-group">
+													Motors needed : <SetMotor drizzle={this.props.drizzle}	drizzleState={this.state.drizzleState}/>
+												</div>
+												<div className="form-group">
+													Doors needed : <SetDoor drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
+												</div>
+												<div className="form-group">
+													Buttons needed : <SetButton drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
+												</div>
+												<div className="form-group">
+													Displays needed : <SetDisplay drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-
 							<div className="panel panel-default">
 								<div className="panel-heading">
 									<h4 className="panel-title">
@@ -146,51 +99,30 @@ class App extends Component {
 											data-toggle="collapse"
 											data-parent="#accordion"
 											href="#collapseTwo"
-										>
-											<span className="glyphicon glyphicon-th-list"> </span>
-											DISPLAY ORDER
-										</a>
+											><span className="glyphicon glyphicon-th-list"> </span>DISPLAY ORDER</a>
 									</h4>
 								</div>
 								<div id="collapseTwo" className="panel-collapse collapse">
-									<div className="panel-body">
+								<div className="panel-body">
 										<div className="row">
 											<div className="col-md-12">
 												<div className="form-group">
-													<ReadController
-														drizzle={this.props.drizzle}
-														drizzleState={this.state.drizzleState}
-													/>
+													<ReadController drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
 												</div>
 												<div className="form-group">
-													<ReadCage
-														drizzle={this.props.drizzle}
-														drizzleState={this.state.drizzleState}
-													/>
+													<ReadCage drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
 												</div>
 												<div className="form-group">
-													<ReadMotor
-														drizzle={this.props.drizzle}
-														drizzleState={this.state.drizzleState}
-													/>
+													<ReadMotor drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
 												</div>
 												<div className="form-group">
-													<ReadDoor
-														drizzle={this.props.drizzle}
-														drizzleState={this.state.drizzleState}
-													/>
+													<ReadDoor drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
 												</div>
 												<div className="form-group">
-													<ReadButton
-														drizzle={this.props.drizzle}
-														drizzleState={this.state.drizzleState}
-													/>
+													<ReadButton drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
 												</div>
 												<div className="form-group">
-													<ReadDisplay
-														drizzle={this.props.drizzle}
-														drizzleState={this.state.drizzleState}
-													/>
+													<ReadDisplay drizzle={this.props.drizzle} drizzleState={this.state.drizzleState}/>
 												</div>
 											</div>
 										</div>
@@ -199,7 +131,7 @@ class App extends Component {
 							</div>
 						</div>
 					</div>
-				</div>
+				</div>	
 			</div>
 		);
 	}
